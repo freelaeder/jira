5fcf8b270a4c87ea423302092b74c5f470c88d37
freelaeder <yanglo67@qq.com>
2022-10-17 20:17:03 +0800

    定义登录接口，实现简单表单登录

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,43 @@
+import React, {FormEvent, useEffect} from 'react';
+// 导入 服务器地址
+const apiUrl = process.env.REACT_APP_API_URL
+
+const LoginScreen = () => {
+    const login = (param: { username: string, password: string }) => {
+        fetch(`${apiUrl}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(param)
+        }).then(async (response) => {
+            if (response.ok) {
+
+            }
+        })
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        // 阻止默认行为
+        event.preventDefault()
+        const username = (event.currentTarget.elements[0] as HTMLInputElement).value
+        const password = (event.currentTarget.elements[1] as HTMLInputElement).value
+        console.log(username, password)
+        login({username, password})
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">username</label>
+                <input type="text " id={'username'}/>
+            </div>
+            <div>
+                <label htmlFor="password">password</label>
+                <input type="text " id={'password'}/>
+            </div>
+            <button type={"submit"}>login</button>
+        </form>
+    );
+};
+
+export default LoginScreen;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -60,3 +60,19 @@ export const useDebounce = <V>(value: V, delay?: number) => {
     }, [value, delay])
     return debounceValue
 }
+
+const useArray = <T>(initialArray:T[]) =>{
+    const [value,setValue] = useState(initialArray)
+    return {
+        value,
+        setValue,
+        add:(item:T) => setValue([...value,item]),
+        clear:() =>setValue([]),
+        removeIndex:(index:number) => {
+            const copy = [...value]
+            copy.splice(index,1)
+            setValue(copy)
+
+        }
+    }
+}
