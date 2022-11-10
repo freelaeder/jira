954b8c4c8853f9b767a4d236957398d16f7ba72e
freelaeder <yanglo67@qq.com>
2022-11-10 15:15:59 +0800

    使用props的方式传递projectModalOpen的状态控制编辑项目组件的显示与隐藏

---
+++ src/components/project-popover.tsx
@@ -0,0 +1,40 @@
+import {Button, Divider, List, Popover, Typography} from "antd";
+import {useProjects} from "../utils/project";
+import styled from "@emotion/styled";
+import {ButtonNoPadding} from "./lib";
+
+// 气泡卡片 鼠标移动项目 展示的卡片
+// https://ant.design/components/popover-cn/#header
+export const ProjectPopover = (props: { setProjectModalOpen: (isOpen: boolean) => void }) => {
+    // 获取收藏项目列表
+    const {data: projects, isLoading} = useProjects()
+    const pinnedProjects = projects?.filter(item => item.pin)
+
+    const content = (
+        <ContentContainer>
+            <Typography.Text type={'secondary'}>收藏项目</Typography.Text>
+            {/*收藏项目列表*/}
+            <List>
+                {
+                    pinnedProjects?.map(project => <List.Item key={project.id}>
+                        <List.Item.Meta key={project.id} title={project.name}/>
+                    </List.Item>)
+                }
+            </List>
+            {/*    分割线*/}
+            <Divider/>
+            {/*    创建项目*/}
+            <ButtonNoPadding onClick={() => props.setProjectModalOpen(true)} type={'link'}> 创建项目 </ButtonNoPadding>
+        </ContentContainer>
+    )
+    return (
+        <Popover placement={'bottom'} content={content}>
+            <span>项目</span>
+        </Popover>
+    )
+}
+
+const ContentContainer = styled.div`
+  min-width: 30rem;
+`
+
